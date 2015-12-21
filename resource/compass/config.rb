b17d92bb037441cce745d276e89a437f58aa83c4
Martin Geisse <martingeisse@googlemail.com>
2015-12-21 20:18:36 +0100

    cleanup

--- resource/compass/config.rb
+++ resource/compass/config.rb
@@ -2,7 +2,7 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "../../src/main/java/name/martingeisse/wicket-my-components/application/page"
+css_dir = "../../src/main/java/name/martingeisse/wicket/demo_app/application/page"
 sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "javascripts"
